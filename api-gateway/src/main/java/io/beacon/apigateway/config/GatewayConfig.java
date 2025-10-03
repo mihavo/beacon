456ed64184a0bef404956772148137f0e587f2b3
Michael Volakis <mivolakis@gmail.com>
2025-10-03 22:31:02 +0300

    feat: add eureka config

---
+++ api-gateway/src/main/java/io/beacon/apigateway/config/GatewayConfig.java
@@ -0,0 +1,15 @@
+package io.beacon.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes().build();
+    }
+}
