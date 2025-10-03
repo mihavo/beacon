873268d926b5ed7f398803bd6acc16492e89e3ff
Michael Volakis <mivolakis@gmail.com>
2025-10-03 23:24:53 +0300

    feat: add route config

--- api-gateway/src/main/java/io/beacon/apigateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/io/beacon/apigateway/config/GatewayConfig.java
@@ -1,7 +1,10 @@
 package io.beacon.apigateway.config;
 
+import java.util.function.Function;
 import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.gateway.route.builder.UriSpec;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +13,22 @@ public class GatewayConfig {
 
     @Bean
     public RouteLocator routes(RouteLocatorBuilder builder) {
-        return builder.routes().build();
+      return builder.routes().route("auth-service",
+              r -> r.path("/auth/**").filters(getDefaultFilters()).uri("lb://auth-service"))
+          .route(
+              r -> r.path("/location/**").filters(getDefaultFilters()).uri("lb://location-service"))
+          .route(r -> r.path("/maps/**").filters(getDefaultFilters()).uri("lb://maps-service"))
+          .route(r -> r.path("/users/**").filters(getDefaultFilters()).uri("lb://user-service"))
+          .route(
+              r -> r.path("/history/**").filters(getDefaultFilters()).uri("lb://history-service"))
+          .route(
+              r -> r.path("/geofence/**").filters(getDefaultFilters()).uri("lb://geofence-service"))
+          .route(r -> r.path("/notifications/**").filters(getDefaultFilters())
+              .uri("lb://notification-service"))
+          .build();
     }
+
+  private static Function<GatewayFilterSpec, UriSpec> getDefaultFilters() {
+    return f -> f.stripPrefix(1);
+  }
 }
