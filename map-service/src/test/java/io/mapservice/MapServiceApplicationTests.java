2f48f605133e8977016ff2fa387a15651011cdcd
Michael Volakis <mivolakis@gmail.com>
2025-10-03 16:16:04 +0300

    feat: setup for map service

---
+++ map-service/src/test/java/io/mapservice/MapServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mapservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
