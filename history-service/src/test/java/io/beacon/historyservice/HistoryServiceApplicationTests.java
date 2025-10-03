31e79add8ca9447283b92544abbd73b6c19d74e3
Michael Volakis <mivolakis@gmail.com>
2025-10-03 16:36:02 +0300

    feat: setup for history service

---
+++ history-service/src/test/java/io/beacon/historyservice/HistoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.beacon.historyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
