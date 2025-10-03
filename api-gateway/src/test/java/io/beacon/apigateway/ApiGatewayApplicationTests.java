c7d9f7380e85bfe6eb6af8540ae3e2aa4000e1df
Michael Volakis <mivolakis@gmail.com>
2025-10-03 18:22:56 +0300

    chore: added api gateway service

---
+++ api-gateway/src/test/java/io/beacon/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.beacon.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
