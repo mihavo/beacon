c60aee62a9a04445b87c87669ada1eedea1e26f2
Michael Volakis <mivolakis@gmail.com>
2025-10-03 16:23:29 +0300

    feat: setup for geofence service

---
+++ geofence-service/src/main/java/io/beacon/geofenceservice/GeofenceServiceApplication.java
@@ -0,0 +1,13 @@
+package io.beacon.geofenceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeofenceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeofenceServiceApplication.class, args);
+    }
+
+}
---
+++ geofence-service/src/test/java/io/beacon/geofenceservice/GeofenceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.beacon.geofenceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeofenceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
