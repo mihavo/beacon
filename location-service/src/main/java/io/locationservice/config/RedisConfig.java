bf76beb2abff14f65ff9f41d21363590a6a15b11
Michael Volakis <mivolakis@gmail.com>
2025-10-05 14:38:25 +0300

    fix(location): redis reference resolver fix

--- location-service/src/main/java/io/locationservice/config/RedisConfig.java
+++ location-service/src/main/java/io/locationservice/config/RedisConfig.java
@@ -3,7 +3,6 @@ package io.locationservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
@@ -13,16 +12,11 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
 
     @Bean
-    public ReactiveRedisConnectionFactory connectionFactory() {
-        return new LettuceConnectionFactory();
-    }
-
-    @Bean
     public ReactiveRedisTemplate<String, Object> redisTemplate(ReactiveRedisConnectionFactory factory) {
         var keySerializer = new StringRedisSerializer();
         var valueSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
         RedisSerializationContext.RedisSerializationContextBuilder<String, Object> builder = RedisSerializationContext.newSerializationContext(keySerializer);
-        builder.value(valueSerializer);
-        return new ReactiveRedisTemplate<>(factory, builder.build());
+        RedisSerializationContext<String, Object> context = builder.value(valueSerializer).build();
+        return new ReactiveRedisTemplate<>(factory, context);
     }
 }
