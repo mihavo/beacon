110b193c086075ae87bd7f624e14bd5dccd13ac2
Michael Volakis <mivolakis@gmail.com>
2025-10-10 18:20:33 +0300

    fix(user): num of connections not showing in response

--- user-service/src/main/java/io/beacon/userservice/connections/dto/ConnectionsInfo.java
+++ user-service/src/main/java/io/beacon/userservice/connections/dto/ConnectionsInfo.java
@@ -1,5 +1,6 @@
 package io.beacon.userservice.connections.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 public record ConnectionsInfo(List<UserInfo> connections) {
@@ -8,6 +9,7 @@ public record ConnectionsInfo(List<UserInfo> connections) {
     connections = connections == null ? List.of() : List.copyOf(connections);
   }
 
+  @JsonProperty("numOfConnections")
   public int numOfConnections() {
     return connections.size();
   }
