78859700fb0d82d1b7281dd0cb6af4d1124e3013
Michael Volakis <mivolakis@gmail.com>
2025-10-15 15:04:16 +0300

    refactor(user): remove self jwt config, add dependency to security-core

--- user-service/src/main/java/io/beacon/userservice/config/SecurityConfig.java
+++ user-service/src/main/java/io/beacon/userservice/config/SecurityConfig.java
@@ -1,42 +1,30 @@
 package io.beacon.userservice.config;
 
-import io.beacon.userservice.security.JwtAuthEntryPoint;
-import io.beacon.userservice.security.JwtAuthManager;
-import io.beacon.userservice.security.JwtAuthenticationFailureHandler;
-import io.beacon.userservice.security.JwtAuthenticationToken;
+import io.beacon.security.config.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.ServerAuthenticationEntryPoint;
 import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
-import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
+@Import(SecurityAutoConfiguration.class)
 public class SecurityConfig {
 
   @Bean
   public SecurityWebFilterChain filterChain(ServerHttpSecurity http,
-      JwtAuthManager jwtAuthManager) {
-    AuthenticationWebFilter filter = new AuthenticationWebFilter(jwtAuthManager);
-    filter.setServerAuthenticationConverter(exchange -> {
-      String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
-      if (authHeader != null && authHeader.startsWith("Bearer ")) {
-        String token = authHeader.substring(7);
-        return Mono.just(new JwtAuthenticationToken(token, null));
-      }
-      return Mono.empty();
-    });
-    filter.setAuthenticationFailureHandler(new JwtAuthenticationFailureHandler());
-
+      AuthenticationWebFilter filter, ServerAuthenticationEntryPoint entryPoint) {
     return http.authorizeExchange(exchanges -> exchanges
             .pathMatchers("/healthcheck/**").permitAll().anyExchange().authenticated())
         .csrf(ServerHttpSecurity.CsrfSpec::disable)
         .addFilterAt(filter, SecurityWebFiltersOrder.AUTHENTICATION)
-        .exceptionHandling(exceptionHandlingSpec -> exceptionHandlingSpec.authenticationEntryPoint(
-            new JwtAuthEntryPoint()))
+        .exceptionHandling(
+            exceptionHandlingSpec -> exceptionHandlingSpec.authenticationEntryPoint(entryPoint))
         .build();
   }
 
