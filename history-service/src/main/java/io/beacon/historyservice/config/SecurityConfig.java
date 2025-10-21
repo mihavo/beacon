96122b1e1681d24ae2aaa0057e09f6f710be77fc
Michael Volakis <mivolakis@gmail.com>
2025-10-21 12:47:14 +0300

    feat(all): add logger config for other services

--- history-service/src/main/java/io/beacon/historyservice/config/SecurityConfig.java
+++ history-service/src/main/java/io/beacon/historyservice/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.ServerAuthenticationEntryPoint;
@@ -22,7 +21,6 @@ public class SecurityConfig {
     return http.authorizeExchange(exchanges -> exchanges
             .pathMatchers("/actuator/**").permitAll().anyExchange().authenticated())
         .csrf(ServerHttpSecurity.CsrfSpec::disable)
-        .addFilterAt(filter, SecurityWebFiltersOrder.AUTHENTICATION)
         .exceptionHandling(
             exceptionHandlingSpec -> exceptionHandlingSpec.authenticationEntryPoint(entryPoint))
         .build();
