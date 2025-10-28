dda955b77220fe7553004bc7eef596b70302d57b
Michael Volakis <mivolakis@gmail.com>
2025-10-28 11:43:41 +0200

    feat(map): map service bootstrap

--- location-service/src/test/load/stress_workflow.js
+++ location-service/src/test/load/stress_workflow.js
@@ -1,5 +1,5 @@
 import http from "k6/http";
-import {check, sleep} from "k6";
+import { check, sleep } from "k6";
 import faker from "k6/x/faker";
 
 const LOCATIONS_PER_USER = 10;
@@ -99,7 +99,7 @@ export default function (data) {
   }
   sleep(0.1);
 
-  sleep(1);
+  sleep(0.1);
 }
 
 function getRandomClusterCoordinates() {
