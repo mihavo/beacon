8a127db0b531f8807726dde25703c352a48cad51
Michael Volakis <mivolakis@gmail.com>
2025-10-28 19:52:56 +0200

    fix(map): fix timestamp conversion

--- map-service/src/main/java/io/beacon/mapservice/mappers/LocationMapper.java
+++ map-service/src/main/java/io/beacon/mapservice/mappers/LocationMapper.java
@@ -2,6 +2,7 @@ package io.beacon.mapservice.mappers;
 
 import io.beacon.events.LocationEvent;
 import io.beacon.mapservice.models.UserLocation;
+import java.time.Instant;
 import locationservice.LocationServiceOuterClass;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -16,4 +17,8 @@ public interface LocationMapper {
   @Mapping(source = "latitude", target = "coords.latitude")
   @Mapping(source = "longitude", target = "coords.longitude")
   UserLocation toUserLocation(LocationServiceOuterClass.UserLocation grpcLocation);
+
+  default Instant map(long value) {
+    return Instant.ofEpochMilli(value);
+  }
 }
