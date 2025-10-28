6760e05e69219891f28aa487b0936bfd6af2ee45
Michael Volakis <mivolakis@gmail.com>
2025-10-28 13:34:09 +0200

    feat(map): create controller, stream endpoint

---
+++ map-service/src/main/java/io/beacon/mapservice/controller/MapController.java
@@ -0,0 +1,30 @@
+package io.beacon.mapservice.controller;
+
+import io.beacon.events.LocationEvent;
+import io.beacon.mapservice.models.BoundingBox;
+import io.beacon.mapservice.service.MapService;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping
+public class MapController {
+
+  private final MapService mapService;
+
+  @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+  public Flux<ServerSentEvent<LocationEvent>> stream(@RequestParam("minLat") double minLat, @RequestParam("maxLat") double maxLat,
+      @RequestParam("minLon") double minLon, @RequestParam("maxLon") double maxLon) {
+    BoundingBox boundingBox = new BoundingBox(minLon, minLat, maxLon, maxLat);
+    return mapService.subscribe(UUID.randomUUID().toString(), boundingBox)
+        .map(event -> ServerSentEvent.<LocationEvent>builder().data(event).build());
+  }
+}
--- map-service/src/main/java/io/beacon/mapservice/service/MapService.java
+++ map-service/src/main/java/io/beacon/mapservice/service/MapService.java
@@ -1,21 +1,28 @@
 package io.beacon.mapservice.service;
 
 import io.beacon.events.LocationEvent;
+import io.beacon.mapservice.models.BoundingBox;
 import io.beacon.mapservice.router.EventRouter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class MapService {
+
   private final EventRouter router;
 
   public Mono<Void> processLocation(LocationEvent event) {
     return router.dispatch(event)
         .doOnSuccess(v -> log.debug("Dispatched location event for user {}", event.userId()))
         .doOnError(e -> log.error("Failed to dispatch location event for user {}", event.userId(), e));
+  }
+
+  public Flux<LocationEvent> subscribe(String clientId, BoundingBox bbox) {
+    return router.subscribe(clientId, bbox);
   }
 }
