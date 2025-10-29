3bd9e4f2c3d318c0097102981ac64687c0e50ace
Michael Volakis <mivolakis@gmail.com>
2025-10-29 22:53:25 +0200

    refactor(location): refactor stream processing in publish service to include redis geospatial updates

--- location-service/src/main/java/io/beacon/locationservice/publish/PublishService.java
+++ location-service/src/main/java/io/beacon/locationservice/publish/PublishService.java
@@ -4,11 +4,13 @@ import io.beacon.locationservice.location.eviction.EvictionService;
 import io.beacon.locationservice.request.PublishLocationRequest;
 import io.beacon.locationservice.utils.CacheUtils;
 import io.beacon.security.utils.AuthUtils;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.geo.Point;
 import org.springframework.data.redis.connection.stream.MapRecord;
 import org.springframework.data.redis.connection.stream.RecordId;
 import org.springframework.data.redis.connection.stream.StreamRecords;
@@ -27,17 +29,41 @@ public class PublishService {
 
   public Flux<RecordId> publish(Set<PublishLocationRequest> input) {
     Mono<UUID> futureUserId = AuthUtils.getCurrentUserId();
-    return futureUserId.flatMapMany(userId -> Flux.fromIterable(input).flatMap(request -> {
-      String streamKey = CacheUtils.buildLocationStreamKey(userId);
-      Map<String, Object> fields =
-          Map.of("lat", request.coords().latitude().toString(), "lon", request.coords().longitude().toString(),
-              "capturedAt", request.capturedAt());
-      MapRecord<String, String, Object> record =
-          StreamRecords.newRecord().in(streamKey).ofMap(fields);
-      return redisTemplate.opsForStream()
-          .add(record)
-          .doOnNext((publishedId) -> log.info("Published location for record {}", publishedId))
-          .flatMap(id -> evictionService.evaluateEviction(userId).thenReturn(id));
-    }));
+
+    return futureUserId.flatMapMany(userId -> {
+      Flux<RecordId> streamRecords = Flux.fromIterable(input)
+          .flatMap(request -> {
+            double lat = request.coords().latitude();
+            double lon = request.coords().longitude();
+
+            String streamKey = CacheUtils.buildLocationStreamKey(userId);
+            Map<String, Object> fields = Map.of(
+                "lat", lat,
+                "lon", lon,
+                "capturedAt", request.capturedAt()
+            );
+
+            MapRecord<String, String, Object> record =
+                StreamRecords.newRecord().in(streamKey).ofMap(fields);
+
+            return redisTemplate.opsForStream()
+                .add(record)
+                .doOnNext(id -> log.info("Published location {}", id))
+                .flatMap(id -> evictionService.evaluateEviction(userId).thenReturn(id));
+          });
+
+      PublishLocationRequest lastLocation = input.stream()
+          .max(Comparator.comparing(PublishLocationRequest::capturedAt))
+          .orElseThrow();
+
+      Mono<Long> geoUpdate = redisTemplate.opsForGeo()
+          .add(CacheUtils.getLocationGeospatialKey(),
+              new Point(lastLocation.coords().longitude(), lastLocation.coords().latitude()),
+              userId.toString());
+
+      return streamRecords.collectList()
+          .flatMapMany(records -> geoUpdate.thenMany(Flux.fromIterable(records)));
+    });
   }
 }
+
--- location-service/src/main/java/io/beacon/locationservice/utils/CacheUtils.java
+++ location-service/src/main/java/io/beacon/locationservice/utils/CacheUtils.java
@@ -8,18 +8,22 @@ import lombok.experimental.UtilityClass;
 public class CacheUtils {
 
   public String buildLocationStreamKey(UUID userId) {
-        return String.format("locations:%s", userId);
-    }
+    return String.format("locations:%s", userId);
+  }
+
+  public String getLocationGeospatialKey() {
+    return "user:locations";
+  }
 
   public UUID extractUserId(String recordKey) {
-        return UUID.fromString(recordKey.substring("locations:".length()));
-    }
+    return UUID.fromString(recordKey.substring("locations:".length()));
+  }
 
-    public Instant extractCapturedAtTimestamp(String recordId) {
-        long millis = Long.parseLong(recordId.split("-")[0]);
-        long nanos = Long.parseLong(recordId.split("-")[1]);
-        return Instant.ofEpochMilli(millis).plusNanos(nanos);
-    }
+  public Instant extractCapturedAtTimestamp(String recordId) {
+    long millis = Long.parseLong(recordId.split("-")[0]);
+    long nanos = Long.parseLong(recordId.split("-")[1]);
+    return Instant.ofEpochMilli(millis).plusNanos(nanos);
+  }
 
   public String getFriendshipListKey(String userId) {
     return String.format("users:%s:friends", userId);
