5680f09e1eeb7a08032c2c7d2f5f2a6e829c6817
Michael Volakis <mivolakis@gmail.com>
2025-11-02 23:24:28 +0200

    refactor(load): wip load test for map streams

---
+++ location-service/src/test/load/map_stream_stress_workflow.js
@@ -0,0 +1,82 @@
+import http from "k6/http";
+import {vu} from "k6/execution";
+import sse from 'k6/x/sse';
+import papaparse from "https://jslib.k6.io/papaparse/5.1.1/index.js";
+import {SharedArray} from "k6/data";
+
+const baseUrl = "http://localhost:8080";
+const testUrl = baseUrl + "/maps";
+const authUrl = baseUrl + "/auth/login";
+
+// AUTH Method 1: Single User token from environment
+// const token = __ENV.AUTH_TOKEN;
+//
+// if (!token) {
+//   throw new Error("AUTH_TOKEN environment variable is not set!");
+// }
+
+//AUTH Method 2: 10k Users with creds from file in data/maxcreds.csv
+
+const users_limit = 100;
+
+const bbox = {
+  minLon: '1.5', maxLon: '2.8', minLat: '45.3', maxLat: '50.3'
+}
+
+const filePath = './data/maxcreds.csv'
+
+const users = new SharedArray("Logins", function () {
+  return papaparse.parse(open(filePath), {header: true}).data.slice(0, users_limit);
+});
+
+export const options = {
+  vus: users.length, duration: '120s'
+};
+
+export function setup() {
+  return users.map((user) => {
+    console.log("Authenticating VU" + vu.idInTest + " / username: ", user.username,);
+    const res = http.post(authUrl, JSON.stringify({
+      username: user.username, password: user.password,
+    }), {headers: {"Content-Type": "application/json"}},);
+
+    if (res.status !== 200) {
+      throw new Error(`Failed to login user ${user.username}, reason:  ${res.body}`,);
+    }
+    console.log(`Authenticated User:  ${user.username}`);
+    return {
+      ...user, token: res.json("token"),
+    };
+  });
+}
+
+export default function (data) {
+  const user = data[__VU - 1];
+  console.log("Executing for VU: " + vu.idInTest + " / username: ", user.username);
+
+  const params = {
+    headers: {
+      "Content-Type": "application/json", Authorization: `Bearer ${user.token}`,
+    },
+  };
+
+  const url = `${testUrl}/subscribe?minLon=${bbox.minLon}&maxLon=${bbox.maxLon}&minLat=${bbox.minLat}&maxLat=${bbox.maxLat}`;
+
+  const response = sse.open(url, params, function (client) {
+    client.on('open', function open() {
+      console.log('connected')
+    })
+
+    client.on('event', function (event) {
+      console.log(`event id=${event.id}, name=${event.name}, data=${event.data}`)
+      if (parseInt(event.id) === 4) {
+        client.close()
+      }
+    })
+
+    client.on('error', function (e) {
+      console.log('An unexpected error occurred: ', e.error())
+    })
+  })
+
+}
