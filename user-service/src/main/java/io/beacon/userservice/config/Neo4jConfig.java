27eb3b400e67fc287adbf8bb1cbabd85397aa543
Michael Volakis <mivolakis@gmail.com>
2025-11-22 12:10:50 +0200

    fix(user): add initialization of fulltext index on user service startup

--- user-service/src/main/java/io/beacon/userservice/config/Neo4jConfig.java
+++ user-service/src/main/java/io/beacon/userservice/config/Neo4jConfig.java
@@ -2,6 +2,8 @@ package io.beacon.userservice.config;
 
 import org.neo4j.cypherdsl.core.renderer.Dialect;
 import org.neo4j.driver.Driver;
+import org.neo4j.driver.Session;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.core.ReactiveDatabaseSelectionProvider;
@@ -20,5 +22,19 @@ public class Neo4jConfig {
   public ReactiveNeo4jTransactionManager reactiveTransactionManager(Driver driver,
       ReactiveDatabaseSelectionProvider databaseNameProvider) {
     return new ReactiveNeo4jTransactionManager(driver, databaseNameProvider);
+  }
+
+  @Bean
+  public ApplicationRunner createFulltextIndex(Driver driver) {
+    return args -> {
+      try (Session session = driver.session()) {
+        session.run("""
+                CREATE FULLTEXT INDEX userSearch
+                IF NOT EXISTS
+                FOR (n:User)
+                ON EACH [n.firstName, n.username]
+            """);
+      }
+    };
   }
 }
