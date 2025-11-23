c455aa4ab2931f4ffbf849a022134d2ddf80eb01
Michael Volakis <mivolakis@gmail.com>
2025-11-23 20:11:36 +0200

    fix(history): fix jts location not persisting in history records

--- history-service/src/main/java/io/beacon/historyservice/locations/mappers/LocationMapper.java
+++ history-service/src/main/java/io/beacon/historyservice/locations/mappers/LocationMapper.java
@@ -4,14 +4,22 @@ import io.beacon.events.LocationEvent;
 import io.beacon.historyservice.locations.entity.LocationHistory;
 import io.beacon.historyservice.locations.entity.LocationHistoryId;
 import java.util.UUID;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.PrecisionModel;
 
 public final class LocationMapper {
 
+  static GeometryFactory geometry = new GeometryFactory(new PrecisionModel(), 4326);
+
   public static LocationHistory toLocationHistory(LocationEvent event) {
-    LocationHistoryId id = LocationHistoryId.builder()
-        .userId(UUID.fromString(event.userId()))
-        .timestamp(event.timestamp()).build();
+    LocationHistoryId id =
+        LocationHistoryId.builder().userId(UUID.fromString(event.userId())).timestamp(event.timestamp()).build();
     return LocationHistory.builder()
-        .id(id).latitude(event.latitude()).longitude(event.longitude()).build();
+        .id(id)
+        .latitude(event.latitude())
+        .longitude(event.longitude())
+        .location(geometry.createPoint(new Coordinate(event.longitude(), event.latitude())))
+        .build();
   }
 }
--- mobile-client/beacon/app/private/_layout.tsx
+++ mobile-client/beacon/app/private/_layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function PrivateTabsLayout() {
     return (
-        <NativeTabs>
+        <NativeTabs minimizeBehavior={"onScrollUp"}>
             <NativeTabs.Trigger name="maps">
                 <Label>Maps</Label>
                 <Icon sf={"map"}></Icon>
