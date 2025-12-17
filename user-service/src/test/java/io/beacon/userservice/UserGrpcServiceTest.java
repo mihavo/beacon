f9cc2ff4078a931a6cc663790704d6dd11edd7e8
Michael Volakis <mivolakis@gmail.com>
2025-12-17 23:56:19 +0200

    test(user): add first test case for create+retrieve

--- user-service/src/test/java/io/beacon/userservice/UserGrpcServiceTest.java
+++ user-service/src/test/java/io/beacon/userservice/UserGrpcServiceTest.java
@@ -2,9 +2,12 @@ package io.beacon.userservice;
 
 import io.beacon.userservice.user.entity.User;
 import net.devh.boot.grpc.client.inject.GrpcClient;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import userservice.UserServiceGrpc;
 import userservice.UserServiceOuterClass;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.AssertionsKt.assertNotNull;
 
 public class UserGrpcServiceTest extends UserServiceApplicationTests {
 
@@ -20,6 +23,17 @@ public class UserGrpcServiceTest extends UserServiceApplicationTests {
         .setPasswordHash(user.getPassword())
         .build();
 
-    UserServiceOuterClass.CreateUserResponse userResponse = userServiceStub.createUser(createRequest);
+    userServiceStub.createUser(createRequest);
+
+    UserServiceOuterClass.GetUserByUsernameRequest retrieveRequest = UserServiceOuterClass.GetUserByUsernameRequest.newBuilder()
+        .setUsername(createRequest.getUsername())
+        .build();
+
+    UserServiceOuterClass.GetUserByUsernameResponse retrievalResponse = userServiceStub.getUserByUsername(retrieveRequest);
+
+    assertNotNull(retrievalResponse.getId());
+    assertEquals(createRequest.getUsername(), retrievalResponse.getUsername());
+    assertEquals(createRequest.getFullName(), retrievalResponse.getFullName());
+    assertEquals(createRequest.getPasswordHash(), retrievalResponse.getPasswordHash());
   }
 }
