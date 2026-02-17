3fe5aee4fd3d7c51e29b4ed5b87a67bf94c44a02
Michael Volakis <mivolakis@gmail.com>
2026-02-17 19:13:24 +0200

    test(geofence): use annotation for auto configuring in-memory db for tests

--- geofence-service/src/test/java/io/beacon/geofenceservice/GeofenceServiceApplicationTests.java
+++ geofence-service/src/test/java/io/beacon/geofenceservice/GeofenceServiceApplicationTests.java
@@ -2,17 +2,14 @@ package io.beacon.geofenceservice;
 
 import io.beacon.geofenceservice.clients.AuthGrpcClient;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@SpringBootTest(properties = {
-    "spring.datasource.url=jdbc:h2:mem:testdb",
-    "spring.datasource.driver-class-name=org.h2.Driver",
-    "spring.jpa.hibernate.ddl-auto=create-drop",
-    "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect"
-})
+@SpringBootTest
+@AutoConfigureTestDatabase()
 @EmbeddedKafka
 @DirtiesContext
 class GeofenceServiceApplicationTests {
