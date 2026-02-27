6edd8a1820aee24916a0a469c1c30a29aef5505e
Michael Volakis <mivolakis@gmail.com>
2026-02-27 11:24:51 +0200

    test(geofence): add it test for geofences

--- geofence-service/src/test/java/io/beacon/geofenceservice/service/GeofenceServiceTest.java
+++ geofence-service/src/test/java/io/beacon/geofenceservice/service/GeofenceServiceTest.java
@@ -10,6 +10,7 @@ import io.beacon.geofenceservice.entity.Geofence;
 import io.beacon.geofenceservice.repository.GeofenceRepository;
 import io.beacon.geofenceservice.utils.TestGeofenceUtils;
 import io.beacon.location.TestLocationUtils;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +24,7 @@ import java.util.List;
 import java.util.UUID;
 
 import static io.beacon.TestUserConstants.TEST_USER_ID;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
@@ -40,6 +41,12 @@ public class GeofenceServiceTest {
 
     @Autowired
     private GeofenceRepository geofenceRepository;
+
+    @BeforeEach
+    void setUp() {
+        geofenceRepository.deleteAll();
+    }
+
 
     @DisplayName("Should create a new geofence object when a geofence request comes in")
     @WithMockBeaconUser(id = TEST_USER_ID)
@@ -73,5 +80,14 @@ public class GeofenceServiceTest {
         assertEquals(2, responses.size());
         assertEquals(TriggerType.ENTER, responses.get(0).triggerType());
         assertEquals(TriggerType.NEAR, responses.get(1).triggerType());
+    }
+
+    @DisplayName("Should not return non-owned geofences")
+    @WithMockBeaconUser(id = "5a308995-0fb1-4fbb-95f1-e70209466539")
+    @Test
+    public void shouldNotReturnOtherGeofences() {
+        TestGeofenceUtils.givenSampleGeofence(geofenceRepository, null, null);
+        List<GeofenceResponse> responses = geofenceService.getAllGeofences().block();
+        assertThat(responses).isEmpty();
     }
 }
